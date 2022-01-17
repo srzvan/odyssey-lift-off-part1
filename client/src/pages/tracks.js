ecfde2c744d9028c4534b2508148e6c8fe430e40
Răzvan Sbîngu <razvan.sbingu@thinslices.com>
2022-01-17 21:59:15 +0200

    client * query for featured tracks

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,42 @@
-import React from 'react';
-import { Layout } from '../components';
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+import { Layout, QueryResult } from "../components";
+import TrackCard from "../containers/track-card";
+
+export const TRACKS = gql`
+  query getTracks {
+    featuredTracks {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { data, loading, error } = useQuery(TRACKS);
+
+  return (
+    <Layout grid>
+      <QueryResult loading={loading} error={error} data={data}>
+        {data?.featuredTracks?.map(track => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
