90a318ca8eaaf287cbe5880541895ef30cbc33be
Răzvan Sbîngu <razvan.sbingu@thinslices.com>
2022-01-18 12:50:03 +0200

    server * tracks api data source

---
+++ server/src/data-sources/tracks-api.js
@@ -0,0 +1,18 @@
+const { RESTDataSource } = require("apollo-datasource-rest");
+
+class TrackAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
+  }
+
+  getFeaturedTracks() {
+    return this.get("tracks");
+  }
+
+  getAuthor(id) {
+    return this.get(`author/${id}`);
+  }
+}
+
+module.exports = TrackAPI;
