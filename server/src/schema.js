cc33ba71fcc4fccdfd524a9d9f66651f7cfe1827
Răzvan Sbîngu <razvan.sbingu@thinslices.com>
2022-01-18 16:59:53 +0200

    server * update schema - add Module type, update Track to include modules field

--- server/src/schema.js
+++ server/src/schema.js
@@ -9,17 +9,39 @@ const typeDefs = gql`
   "A Track is a collection of Modules that teaches about a specific topic"
   type Track {
     id: ID!
+    "Track length in seconds"
     length: Int
+    "Title of the Track"
     title: String!
+    "Author of the Track"
     author: Author!
+    "URL for Track thumbnail"
     thumbnail: String
+    "Number of Modules that make up the Track"
     modulesCount: Int
+    "The number of times the Track has been viewed"
+    numberOfViews: Int
+    "The Track's complete description (can be in Markdown format)"
+    description: String
+    "The Track's complete array of Modules"
+    modules: [Module!]!
+  }
+
+  "A Module is a single unit of teaching. Multiple Modules compose a Track"
+  type Module {
+    id: ID!
+    "Module title"
+    title: String!
+    "Module length in minutes"
+    length: Int
   }
 
   "An Author is an entity that creates complete Tracks and/or Modules"
   type Author {
     id: ID!
+    "Author full name"
     name: String!
+    "URL for Author photo"
     photo: String
   }
 `;
