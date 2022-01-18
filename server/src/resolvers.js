9328fce64e6cf82406c2b43d1067a760f8168d77
Răzvan Sbîngu <razvan.sbingu@thinslices.com>
2022-01-18 15:55:34 +0200

    server * gracefully handle runtime errors in resolvers

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,23 +1,33 @@
+const { ApolloError } = require("apollo-server");
+
 const resolvers = {
   Query: {
     // Returns an array of featured Tracks used
     // to populate the card grid on the home page
     featuredTracks: (_, __, context) => {
-      const {
-        dataSources: { tracksAPI },
-      } = context;
+      try {
+        const {
+          dataSources: { tracksAPI },
+        } = context;
 
-      return tracksAPI.getFeaturedTracks();
+        return tracksAPI.getFeaturedTracks();
+      } catch (error) {
+        throw new ApolloError(error);
+      }
     },
   },
   Track: {
     author: (parent, _, context) => {
-      const { authorId: id } = parent;
-      const {
-        dataSources: { tracksAPI },
-      } = context;
+      try {
+        const { authorId: id } = parent;
+        const {
+          dataSources: { tracksAPI },
+        } = context;
 
-      return tracksAPI.getAuthor(id);
+        return tracksAPI.getAuthor(id);
+      } catch (error) {
+        throw new ApolloError(error);
+      }
     },
   },
 };
